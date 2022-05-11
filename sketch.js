5992c131812183e92ca3915f40a542fdc3b5d914
L33N4 <104579858+L33N4@users.noreply.github.com>
2022-05-11 10:48:48 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
+let rand
+let button
+let result
+let names = ["Jarvis", "Lexi,", "Sydney", "Lucie", "Max", "Pross", "Holden", "Spencer", "Atlas", "Dante", "Ahmed", "Riley", "Emma", "Maria", "Nate", "Ronald", "John", "Roger", "Connor", "Ryker", "Charles", "David", "Alia", "Catherine", "Maria", "Hadley", "Eli", "Ronald", "Luthor", "Barry", "Aris", "Oliver", "Thea", "Jessica", "Jones", "Byron"]
+let = 0
 function setup() {
-  createCanvas(400, 400);
+  
 }
 
 function draw() {
-  background(220);
+  
+}
+
+function () {
+  
 }
